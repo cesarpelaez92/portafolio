e9354b0fce8677aaae2bad3a731e297258b585b3
Cesar Pelaez <cesar_pelaez92@hotmail.com>
2020-06-24 18:26:01 -0500

    Updates

---
+++ precache-manifest.2ae3c915d38a8dcf34a54982195da8df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ac853c261873fff6ba94de84060783",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "3fb2e7ef6085f1337619",
+    "url": "/portafolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30aa2f0ea61258e3c697",
+    "url": "/portafolio/static/js/2.f96d775e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio/static/js/2.f96d775e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb2e7ef6085f1337619",
+    "url": "/portafolio/static/js/main.b9281fc1.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portafolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
